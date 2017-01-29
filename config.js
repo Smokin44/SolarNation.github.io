e483a628892c655c1724c8cdf32b4f1e74f59876
BuildTools <unconfigured@null.spigotmc.org>
2017-01-29 18:28:41 -0500

    First Map Upload

---
+++ config.js
@@ -0,0 +1,50 @@
+var CONFIG = {
+  "maps": {
+    "TopDown": {
+      "imageFormat": "png",
+      "lastRendered": [
+        1485731534,
+        0,
+        0,
+        0
+      ],
+      "maxZoom": 6,
+      "name": "Solar Nation",
+      "renderView": "isometric",
+      "rotations": [
+        0
+      ],
+      "textureSize": 16,
+      "tileSetGroup": "Eco_City_Craft_isometric_t1",
+      "tileSize": 512,
+      "world": "Eco_City_Craft",
+      "worldName": "Eco_City_Craft"
+    }
+  },
+  "mapsOrder": [
+    "TopDown"
+  ],
+  "tileSetGroups": {
+    "Eco_City_Craft_isometric_t1": {
+      "maxZoom": 6,
+      "tileOffsets": [
+        [
+          14,
+          -57
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ],
+        [
+          0,
+          0
+        ]
+      ]
+    }
+  }
+};
