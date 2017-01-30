4aeec122fc93dce1de6ee9b9a520f093771bf65a
BuildTools <unconfigured@null.spigotmc.org>
2017-01-29 19:17:44 -0500

    Marker Update

--- markers.js
+++ markers.js
@@ -1,12 +1,26 @@
-var MAPCRAFTER_MARKERS = [
-
-	{
-        "id" : "Solar Nation",
-        "name" : "Capitol",
-        "markers" : {
-            "Eco_City_Craft" : [
-                {"pos" : [2185, -1702, 100], "Solar Nation" : "Capitol"},
-            ],
-        },
-    },
+var MAPCRAFTER_MARKERS = 
+[
+  {
+  "id" : "Sectors",
+  "name" : "Sectors",
+  "showDefault" : true,
+  "markers" : {
+      "Eco_City_Craft" : [
+          {"pos" : [2178, -1498, 63],"title" : "Sector 4","text" : "Builder Town",},
+      ],
+  },
+  },	
+  {
+      "id" : "Buildings",
+      "name" : "Buildings",
+      "showDefault" : false,
+      "icon" : "sign.png",
+      "iconSize" : [50, 50],
+      "markers" : {
+          "Eco_City_Craft" : [
+              {"pos" : [2185, -1702, 100], "Solar Nation" : "Capitol","title" : "Capitol Building",},
+              {"pos" : [2288, -1665, 63], "Solar Nation" : "Test","title" : "Library",},
+          ],
+      },
+  },
 ];
