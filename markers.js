17b49bbe42cbc3c953b68d06c6c01137b4eaae0e
BuildTools <unconfigured@null.spigotmc.org>
2017-01-29 22:24:57 -0500

    Update Marker & Sign Picture

--- markers.js
+++ markers.js
@@ -1,25 +1,36 @@
-var MAPCRAFTER_MARKERS = 
+var MAPCRAFTER_MARKERS =
+// Offline 
 [
   {
   "id" : "Sectors",
-  "name" : "Sectors",
+  "name" : "Solar Nation Sectors",
   "showDefault" : true,
   "markers" : {
       "Eco_City_Craft" : [
+          {"pos" : [2185, -1702, 100],"title" : "Capitol","text" : "Capitol",},
           {"pos" : [2178, -1498, 63],"title" : "Sector 4","text" : "Builder Town",},
+          {"pos" : [1980, -1500, 63],"title" : "Sector 5","text" : "Resident Town",},
+          {"pos" : [1980, -1700, 63],"title" : "Sector 6","text" : "Entertainment Town",},
+          {"pos" : [1980, -1900, 63],"title" : "Sector 7","text" : "? Town",},
+          {"pos" : [2180, -1900, 63],"title" : "Sector 8","text" : "Luxury Town",},
+          {"pos" : [2380, -2100, 63],"title" : "Solar Farms","text" : "Farm Sector 1",},
+          {"pos" : [2380, -1900, 63],"title" : "Sector 1","text" : "Farm Sector 2",},
+          {"pos" : [2380, -1700, 63],"title" : "Sector 2","text" : "Farm Sector 3",},
+          {"pos" : [2380, -1500, 63],"title" : "Sector 3","text" : "Farm Sector 4",},
+          {"pos" : [2380, -1300, 63],"title" : "Solar Farms","text" : "Farm Sector 5",},
       ],
   },
   },	
   {
       "id" : "Buildings",
-      "name" : "Buildings",
+      "name" : "Capitol - Buildings",
       "showDefault" : false,
       "icon" : "sign.png",
       "iconSize" : [50, 50],
       "markers" : {
           "Eco_City_Craft" : [
-              {"pos" : [2185, -1702, 100], "Solar Nation" : "Capitol","title" : "Capitol Building",},
-              {"pos" : [2288, -1665, 63], "Solar Nation" : "Test","title" : "Library",},
+              {"pos" : [2185, -1702, 100], "title" : "Capitol Building","text" : "Capitol Building",},
+              {"pos" : [2288, -1665, 63], "title" : "Library","text" : "Library",},
           ],
       },
   },
