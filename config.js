8345bdddf8204f18d7f1fd35accb150f4d596517
BuildTools <unconfigured@null.spigotmc.org>
2017-02-13 18:24:22 -0500

    Major Update
    New map & more views

---
+++ config.js
@@ -0,0 +1,100 @@
+var CONFIG = {
+  "maps": {
+    "Solar_Nation_1_29": {
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1487011252,
+        1487011402,
+        1487011402,
+        1487011402
+      ],
+      "maxZoom": 6,
+      "name": "Jan. 29, 2017",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "SolarNation_1_29_isometric_t1",
+      "tileSize": 384,
+      "world": "SolarNation_1_29",
+      "worldName": "SolarNation_1_29"
+    },
+    "Solar_Nation_2_13": {
+      "defaultZoom": 3,
+      "imageFormat": "png",
+      "lastRendered": [
+        1487011252,
+        1487011402,
+        1487011402,
+        1487011402
+      ],
+      "maxZoom": 6,
+      "name": "Feb. 13, 2017",
+      "renderView": "isometric",
+      "rotations": [
+        0,
+        1,
+        2,
+        3
+      ],
+      "textureSize": 12,
+      "tileSetGroup": "SolarNation_2_13_isometric_t1",
+      "tileSize": 384,
+      "world": "SolarNation_2_13",
+      "worldName": "SolarNation_2_13"
+    }
+  },
+  "mapsOrder": [
+    "Solar_Nation_1_29",
+    "Solar_Nation_2_13"
+  ],
+  "tileSetGroups": {
+    "SolarNation_1_29_isometric_t1": {
+      "maxZoom": 6,
+      "tileOffsets": [
+        [
+          14,
+          -57
+        ],
+        [
+          136,
+          3
+        ],
+        [
+          16,
+          64
+        ],
+        [
+          -106,
+          4
+        ]
+      ]
+    },
+    "SolarNation_2_13_isometric_t1": {
+      "maxZoom": 6,
+      "tileOffsets": [
+        [
+          14,
+          -57
+        ],
+        [
+          136,
+          3
+        ],
+        [
+          16,
+          64
+        ],
+        [
+          -106,
+          4
+        ]
+      ]
+    }
+  }
+};
